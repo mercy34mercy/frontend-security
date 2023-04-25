8056a68a3c6b17b20e6634bcb119f3c375818507
makun <makun@makunnoMac-mini.local>
2023-04-25 13:58:57 +0900

    second commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.end('Hello World!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
